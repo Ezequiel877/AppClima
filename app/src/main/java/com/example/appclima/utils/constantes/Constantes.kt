f6e68e3b8672e27b9a4d3f665154d7bba2588eea
Ezequiel Rodriguez <83467909+Ezequiel877@users.noreply.github.com>
2022-08-01 00:01:08 -0300

    Update Constantes.kt

--- app/src/main/java/com/example/appclima/utils/constantes/Constantes.kt
+++ app/src/main/java/com/example/appclima/utils/constantes/Constantes.kt
@@ -2,6 +2,6 @@ package com.example.appclima.utils.constantes
 
 object constantes {
 
-    var API_KEY="4619174d2b22b1bdb7b87c8f47d9c510"
+    var API_KEY=""
     var URL_BASE="https://api.openweathermap.org/"
 }
